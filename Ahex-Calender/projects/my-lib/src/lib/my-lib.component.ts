89da868de6c43327fcecc9313f80b4176d39ce4d
Shivani Chaurasia <shivani.chaurasia@ahex.co.in>
2023-04-06 11:42:05 +0530

    done some change

--- Ahex-Calender/projects/my-lib/src/lib/my-lib.component.ts
+++ Ahex-Calender/projects/my-lib/src/lib/my-lib.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Day } from '../lib/model/day.model';
+import { MyLibService } from './my-lib.service';
 
 
 @Component({
@@ -8,79 +10,100 @@ import { Component, Input } from '@angular/core';
 
 })
 export class MyLibComponent {
-  givenDate: Date = new Date();
-  @Input() fromDate: any;
-  @Input() toDate: any;
-  formateDate: any;
-  picker: any;
-  data: any;
 
+  @Input() calHeaderColor: string = "rgb(9,97,146)";
+  @Input() calHeaderFontSize: string = "20px";
 
-  constructor() {
-    
-   }
+  public month: any = [0, 2]
+  public monthDays: Day[] = [];
+  showMyContainer: boolean = false;
+  public monthNumber: any;
+  public year!: number;
+  public weekDaysName = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+  currentYear: number;
+  currentMonthIndex: number;
+  getMonth: any;
+  date: Date;
+  selectedDate: any;
+  myInputValue: any;
+  gettingDate: number;
 
+  constructor(public calendarCreator: MyLibService) {
+    this.date = new Date();
+    this.currentYear = this.date.getFullYear();
+    this.currentMonthIndex = this.date.getMonth();
+    this.gettingDate = this.date.getDate();
+    // console.log(this.gettingDate);
+
+  }
   ngOnInit() {
-    this.getDate();
-    this.isItWeekEnd();
+    this.setMonthDays(this.calendarCreator.getCurrentMonth());
+  }
+  onNextMonth(): void {
+    this.monthNumber++;
+    if (this.monthNumber == 13) {
+      this.monthNumber = 1;
+      this.year++;
+    }
+
+    this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
+  }
+
+  onPreviousMonth(): void {
+    this.monthNumber--;
+
+    if (this.monthNumber < 1) {
+      this.monthNumber = 12;
+      this.year--;
+    }
+    this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
 
   }
 
-  getDate() {
-    
-    this.formateDate = this.fromDate.getFullYear() + "-" + ("0" + (this.fromDate.getMonth() + 1)).slice(-2) + "-" + ("0" + this.fromDate.getDate()).slice(-2)
-    console.log(this.formateDate);
+  private setMonthDays(days: Day[]): void {
+    this.monthDays = days;
+    // console.log(this.monthDays);
+    this.monthDays.map((x: any) => {
+      // console.log(x.weekDayNumber);
+      x.disable = this.month.filter((y: any) =>
+       {
+        if(y == x.weekDayNumber){
+        return true;         
+        }else{
+          return false;
+        }
+       })
+    })
+    console.log(this.monthDays);
+
+    this.monthNumber = this.monthDays[0].monthIndex;
+
+    this.year = this.monthDays[0].year;
+
+  }
+
+  getDate(day: Day) {
+    let yr;
+    let mnth;
+    let dt;
+    yr = day.year;
+    mnth = day.monthIndex + 1;
+    dt = day.number;
+    this.selectedDate = dt + '/' + mnth + '/' + yr
+    this.myInputValue = this.selectedDate
+
+    this.showMyContainer = false;
+  }
+
+
+  max(day: any) {
+    let yr;
+    let mnth;
+    let dt;
+    yr = day.year;
+    mnth = day.monthIndex + 1;
+    dt = day.number;
+    this.selectedDate = dt + '/' + mnth + '/' + yr
+
+  }
 }
-isItWeekEnd(){
-    // Everything except weekend days
-    const validate = (dateString: any) => {
-      const day = (new Date(dateString)).getDay();
-      if (day==0 || day==6) { 
-        return false;
-      }
-      return true;
-    }
-    // Sets the value to '' in case of an invalid date
-    (document.querySelector('input') as HTMLInputElement).onchange =(evt:any) => {
-      if (!validate(evt.target.value)) {
-        evt.target.value = '';
-        alert("Weekends are not allowed")
-      }                                                                       
-    }
- }
-
-
-  // isItWeekEnd() {
-  //   var d = new Date();
-  //   var getTot = this.daysInMonth(d.getMonth(), d.getFullYear()); //Get total days in a month
-  //   var sat = new Array();   //Declaring array for inserting Saturdays
-  //   var sun = new Array();   //Declaring array for inserting Sundays
-
-  //   for (var i = 1; i <= getTot; i++) {    //looping through days in month
-  //     var newDate = new Date(d.getFullYear(), d.getMonth(), i)
-  //     if (newDate.getDay() == 0) {   //if Sunday
-  //       sun.push(i);
-
-  //     }
-  //     if (newDate.getDay() == 6) {   //if Saturday
-  //       sat.push(i);
-
-  //     }
-
-  //   }
-  //   console.log(sat);
-  //   console.log(sun);
-
-  // }
-  // daysInMonth(month: any, year: any) {
-  //   return new Date(year, month, 0).getDate();
-  // }
-}
-
-
-
-
-
-
-
-
--- Ahex-Calender/projects/my-lib/src/lib/my-lib.module.ts
+++ Ahex-Calender/projects/my-lib/src/lib/my-lib.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MyLibComponent } from './my-lib.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { MyLibComponent } from './my-lib.component';
     MyLibComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ], 
   exports: [
     MyLibComponent
